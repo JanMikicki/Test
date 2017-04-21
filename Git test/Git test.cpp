5adb7ad06bbd256e8b24602e5d0ff02f07ec478d
Jan Mikicki <Jan Mikicki>
2017-04-21 17:48:04 +0200

    test

--- Git test/Git test.cpp
+++ Git test/Git test.cpp
@@ -1,11 +1,13 @@
 // Git test.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
-
+//#include "stdafx.h"
+#include <iostream>
 
 int main()
 {
+
+	std::cout << "TEST\n";
     return 0;
 }
 
